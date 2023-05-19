0a3d8a2904d58f61581c60ccf314dde133ef7ee1
7ashraf <mohamedxman859@gmail.com>
2023-05-19 19:46:24 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+var createError = require('http-errors');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const mongoose = require('mongoose')
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+
+// view engine setup
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json())
+app.use((req, res, next)=>{
+    console.log(req.path)
+    next()
+})
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+mongoose.connect(process.env.MONGO).then(()=>{
+  app.listen(4000, ()=>{
+      console.log('listening on port 4000')
+      console.log('connected to db')
+  })
+}).catch((error)=>{
+  console.log(error)
+})
+module.exports = app;
