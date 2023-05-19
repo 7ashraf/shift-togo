a6f3cff148efb1e055d213d459048760d3f7a5b4
7ashraf <mohamedxman859@gmail.com>
2023-05-20 02:28:22 +0300

    fix

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-var createError = require('http-errors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const express = require('express')
 require('dotenv').config()
+const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose')
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const helloWorld = require('./routes/helloWorld')
 
 
-// view engine setup
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,12 +21,18 @@ app.use((req, res, next)=>{
     console.log(req.path)
     next()
 })
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+//routes
+app.use('/api/',helloWorld)
 
 mongoose.connect(process.env.MONGO).then(()=>{
-  app.listen(4000, ()=>{
+  app.listen(process.env.PORT, ()=>{
       console.log('listening on port 4000')
       console.log('connected to db')
   })
---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+function App() {
+  const [helloWorld, setHelloWorld] = useState(null)
+  const fetchHello = async()=>{
+    const response = await fetch('/api')
+    try{
+    const json = await response.json()
+    if(response.ok){
+      setHelloWorld(json)
+  }else{
+  }
+    }catch(error){
+      console.log(error)
+
+    }
+    
+}
+  useEffect(() => {
+    
+    
+  fetchHello()
+  
+   
+  }, [helloWorld])
+  
+  
+  return (
+    
+    <div className="App">
+      <h1></h1>
+    </div>
+  );
+}
+
+export default App;
